b91bfe80918e7029935890fc9c5f035deabdce19
Jacob Shing <zhanhoshing@icloud.com>
2026-03-25 13:24:35 +0800

    feat: add workflow to fetch and update CSV data from Google Sheets

--- campus_nav/utils.py
+++ campus_nav/utils.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 from .models import *
 
-NODES_CSV_PATH = "data/NODES.csv"
+NODES_CSV_PATH = "data/nodes.csv"
 NODES_CSV_GOOGLE_SHEET_GID = "1774115251"
-EDGES_CSV_PATH = "data/EDGES.csv"
+EDGES_CSV_PATH = "data/edges.csv"
 EDGES_CSV_GOOGLE_SHEET_GID = "171496836"
 GOOGLE_SHEETS_ID = "2PACX-1vRUqgKeU2uVyHsbVwYAIsMW-OqxgqaBB-GGY8w4thwaozPplOu1bN5rJgLzUPufFrIWL8p-QdaMI4Np"
 
