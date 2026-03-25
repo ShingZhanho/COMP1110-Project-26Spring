17ba798ad4c23dfd1d30b0476b27140f75accd19
Jacob Shing <zhanhoshing@icloud.com>
2026-03-25 09:21:06 +0800

    feat: added utilities for loading CSV

---
+++ campus_nav/utils.py
@@ -0,0 +1,39 @@
+import csv
+from pathlib import Path
+
+from .models import *
+
+NODES_CSV_PATH = "data/NODES.csv"
+EDGES_CSV_PATH = "data/EDGES.csv"
+
+def construct_map_from_csv(nodes_file: str = None, edges_file: str = None) -> CampusMap:
+    """
+    Constructs a CampusMap object from the given CSV files containing nodes and edges information.
+    :param nodes_file: The path to the CSV file containing nodes information. If None, it defaults to NODES_CSV_PATH.
+    :param edges_file: The path to the CSV file containing edges information. If None, it defaults to EDGES_CSV_PATH.
+    :return: The constructed CampusMap object.
+    """
+    if nodes_file is None: nodes_file = Path(__file__).resolve().parent / NODES_CSV_PATH
+    if edges_file is None: edges_file = Path(__file__).resolve().parent / EDGES_CSV_PATH
+
+    campus_map: CampusMap = CampusMap()
+
+    # Read nodes
+    with open(str(nodes_file), mode='r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader) # Skip header
+        for row in reader:
+            node_id, node_description = row
+            campus_map.add_node(Node(node_id, node_description))
+
+    # Read edges
+    with open(str(edges_file), mode='r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        next(reader) # Skip header
+        for row in reader:
+            node1_id, node2_id, edge_type, edge_time_cost = row
+            edge_type = EdgeType(edge_type)
+            edge_time_cost = int(edge_time_cost)
+            campus_map.add_edge(node1_id, node2_id, edge_type, edge_time_cost)
+
+    return campus_map
